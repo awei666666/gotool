305828a930be1fd776048b4a23fcb419106293cb
韩威兵 <a1877361302@163.com>
2022-01-27 10:33:56 +0800

    完善协程工具类

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -1,21 +1,67 @@
 package goroutine
 
 import (
+	"errors"
+	"runtime"
 	"sync"
 )
 
 var wg sync.WaitGroup
 var wgMap = make(map[string]interface{})
-var LockMapObj = &LockMap{i: 8, lock: &sync.RWMutex{}}
+var lockMapObj = &lockMap{i: 8, lock: &sync.RWMutex{}}
+var wgError = make(map[string]Err)
+
+type Err struct {
+	Msg  string
+	Code int // 0程序抛出的异常  1程序运行的时候抛出的异常，比如除以0，数据转换格式错误等
+}
 
 // 增加一个协程
 func AddGo(key string, f func(funcValue ...interface{}) interface{}, funcValue ...interface{}) {
 	wg.Add(1) //添加一个计数
-	 go func(funcValue ...interface{}) {
-		 res := f(funcValue...)
+	go func(funcValue ...interface{}) {
+		defer func() {
+			// 发生宕机时，获取panic传递的上下文并打印
+			err := recover()
+			if err != nil {
+				switch err.(type) {
+				case runtime.Error: // 运行时错误
+					lockMapObj.lock.Lock()
+					wgError[key] = Err{Msg: err.(runtime.Error).Error(), Code: 1}
+					lockMapObj.lock.Unlock()
+				default: // 非运行时错误 程序错误
+					lockMapObj.lock.Lock()
+					wgError[key] = Err{Msg: err.(string), Code: 0}
+					lockMapObj.lock.Unlock()
+				}
+				lockMapObj.set(key, nil)
+				defer wg.Done()
+			}
+		}()
+		res := f(funcValue...)
+		lockMapObj.set(key, res)
 		defer wg.Done()
-		LockMapObj.Set(key, res)
 	}(funcValue...)
+}
+
+func GetValueByKey(key string) (interface{}, error) {
+	value, ok := wgMap[key]
+	if !ok {
+		return nil, errors.New("没有找到相关协程数据")
+	}
+	if value == nil {
+		if err, ok := wgError[key]; ok {
+			var msg string
+			if err.Code == 1 {
+				msg = "运行时的问题:"
+			} else {
+				msg = "程序错误:"
+			}
+			return nil, errors.New(msg + err.Msg)
+		}
+		return nil, nil
+	}
+	return value, nil
 }
 
 //等待协程结束
@@ -24,16 +70,19 @@ func Wait() map[string]interface{} {
 	return wgMap
 }
 
+func GetErrMap() map[string]Err {
+	return wgError
+}
+
 // 使用读写锁 sync.RWMutex
-type LockMap struct {
+type lockMap struct {
 	lock *sync.RWMutex
 	i    int
 }
 
-func (m *LockMap) Set(k string, v interface{}) bool {
+func (m *lockMap) set(k string, v interface{}) bool {
 	m.lock.Lock()
 	defer m.lock.Unlock()
 	wgMap[k] = v
 	return true
 }
-
